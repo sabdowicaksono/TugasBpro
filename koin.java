91abb039cd51d0a9078ee2168d5942927bb3ba89
sabdowicaksono <sabdo_wicaksono@yahoo.com>
2015-04-15 11:21:27 +0700

    Create koin.java

---
+++ koin.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class koin
+{
+	public static void main (String argc [])
+	{
+		
+		int bilrandom = (int) (Math.Random ()(1+0.5));
+		if (bilrandom == 1)
+		{
+			System.out.println("Gambar");
+		}
+		else
+		{
+			System.out.println("Gambar");
+		}
+	}
+}
